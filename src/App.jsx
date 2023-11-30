61e62984ed2639192100313b2aaa976ba5d7193a
alikansoh <alikansoh.dev@gmail.com>
2023-11-30 21:00:27 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import react from 'react';
+import React, { useState } from 'react';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element }) => {
+  const isAuthenticated = localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
 
 export default function App() {
-
   return (
     <>
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+
+          <Route
+            path="/dashboard"
+            element={<PrivateRoute element={<Dashboard />} />}
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
-
-
